1cfdcda03dec5d6a82d6eaa2f3feae80c381931e
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-08-30 14:05:12 -0400

    done

---
+++ arrays.js
@@ -0,0 +1,36 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  new_array = array
+  new_array.unshift(element)
+  return new_array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+ return array[index]
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
